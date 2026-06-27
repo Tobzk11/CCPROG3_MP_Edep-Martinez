6817c23d3d2484b657ab9cec98783ddbffdd59bc
simonjaxith <simonjaxithmartinez77@gmail.com>
2026-06-27 23:02:03 +0800

    Implemented methods for the media classes, incomplete for now

--- src/VideoGame.java
+++ src/VideoGame.java
@@ -1,2 +1,27 @@
 public class VideoGame {
+    private MediaEntry entry;
+    private String platform;
+    private String requiredSpecs;
+    private String developer;
+    private double hoursPlayed;
+
+    public VideoGame(MediaEntry entry, String platform, String requiredSpecs, String developer, double hoursPlayed) {
+        this.entry = entry;
+        this.platform = platform;
+        this.requiredSpecs = requiredSpecs;
+        this.developer = developer;
+        this.hoursPlayed = hoursPlayed;
+    }
+
+    public boolean updateHoursPlayed(double hours) {
+        return true; // change this!
+    }
+
+    public MediaEntry getEntry() {
+        return this.entry;
+    }
+
+    public String getGameDetails() {
+        return "hi"; // change this!
+    }
 }
