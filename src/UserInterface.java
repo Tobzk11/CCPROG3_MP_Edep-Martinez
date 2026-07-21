954d320a5a47382eb15eaa79bf5f47ed1ef40b04
simonjaxith <simonjaxithmartinez77@gmail.com>
2026-07-21 18:22:34 +0800

    Start page created

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,5 +1,12 @@
 import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.*;
 
 public class UserInterface extends Application {
 
@@ -9,6 +16,30 @@ public class UserInterface extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 500, Color.BLACK);
 
+        Image icon = new Image("6192518233113691.jpg"); // images
+        Image bgImage = new Image("main.png");
+
+        ImageView bgImg = new ImageView(bgImage); // background image
+        bgImg.setFitHeight(500);
+        bgImg.setFitWidth(500);
+
+        primaryStage.getIcons().add(icon); // root settings
+        primaryStage.setTitle("MediaVault");
+        primaryStage.setResizable(false);
+
+        Font custom = Font.loadFont(getClass().getResourceAsStream("ByteBounce.ttf"), 75); // title
+        Text title = new Text();
+        title.setText("MediaVault");
+        title.setX(110);
+        title.setY(200);
+        title.setFont(custom);
+        title.setFill(Color.WHITE);
+
+        root.getChildren().addAll(bgImg, title);
+        primaryStage.setScene(scene);
+        primaryStage.show();  // must be last line
     }
 }
