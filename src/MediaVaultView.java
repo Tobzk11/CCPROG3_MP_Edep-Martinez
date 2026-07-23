45e0179d51c9fd3e29f30d6aec2ffba47e871f51
Tobzkii <tobzkii.edep@gmail.com>
2026-07-23 15:37:54 +0800

    Renamed UserInterface to MediaVaultView. Made MediaVaultApp call MediaVaultView instead.

--- src/MediaVaultView.java
+++ src/MediaVaultView.java
@@ -8,7 +8,7 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.*;
 
-public class UserInterface extends Application {
+public class MediaVaultView extends Application {
 
     public static void main(String[] args) {
         launch(args);
