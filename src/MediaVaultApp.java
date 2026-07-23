e224f0ef96f0264a03bb45df4a7f022f1b398853
simonjaxith <simonjaxithmartinez77@gmail.com>
2026-07-23 23:20:08 +0800

    Created controller class, added username input (with validation logic) to UI

--- src/MediaVaultApp.java
+++ src/MediaVaultApp.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 
 public class MediaVaultApp {
     public static void main(String[] args) {
+
         Application.launch(MediaVaultView.class, args);
     }
 }
--- src/MediaVaultView.java
+++ src/MediaVaultView.java
@@ -1,14 +1,22 @@
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import javafx.scene.*;
 
 public class MediaVaultView extends Application {
+
+    private String username;
+    private String fontFam;
 
     public static void main(String[] args) {
         launch(args);
@@ -31,6 +39,7 @@ public class MediaVaultView extends Application {
         primaryStage.setResizable(false);
 
         Font custom = Font.loadFont(getClass().getResourceAsStream("ByteBounce.ttf"), 75); // title
+        fontFam = custom.getFamily();
         Text title = new Text();
         title.setText("MediaVault");
         title.setX(110);
@@ -38,7 +47,33 @@ public class MediaVaultView extends Application {
         title.setFont(custom);
         title.setFill(Color.WHITE);
 
-        root.getChildren().addAll(bgImg, title);
+        TextField user = new TextField();
+        user.setPromptText("Username");
+        user.setLayoutX(175);
+        user.setLayoutY(225);
+
+        Button enter = new Button("Submit");
+        enter.setLayoutX(220);
+        enter.setLayoutY(260);
+        enter.setFont(Font.font(fontFam, 15));
+
+        Label warning = new Label();
+        warning.setLayoutX(178);
+        warning.setLayoutY(290);
+        warning.setFont(Font.font(fontFam, 13));
+        warning.setTextFill(Color.RED);
+
+        enter.setOnAction(event ->
+        {String input = user.getText();
+        if (input.trim().isEmpty())
+            warning.setText("Please enter a valid username");
+        else {
+            username = input;
+            // next stage
+        }
+        });
+
+        root.getChildren().addAll(bgImg, title, user, enter, warning);
         primaryStage.setScene(scene);
         primaryStage.show();  // must be last line
     }
