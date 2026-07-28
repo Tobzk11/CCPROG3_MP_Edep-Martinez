e7312f6c1665e7bde147a6fdbc1dca23c3df0406
simonjaxith <simonjaxithmartinez77@gmail.com>
2026-07-28 16:52:12 +0800

    MediaVaultApp modified to use GUI

--- src/MediaVaultApp.java
+++ src/MediaVaultApp.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * Entry point of the MediaVault program. Its only job is to hand control to
@@ -6,18 +9,18 @@ import javafx.application.Application;
  * start method. The view is responsible for creating the Library, the
  * FileManager, and the MediaVaultController that ties them together.
  */
-public class MediaVaultApp {
 
-    /**
-     * Launches the JavaFX application.
-     * <p>
-     * <b>Postcondition:</b> the JavaFX runtime has started and the MediaVault
-     * window is displayed
-     * </p>
-     *
-     * @param args command line arguments passed through to JavaFX
-     */
+public class MediaVaultApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainview.fxml"));
+
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("MediaVault");
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        Application.launch(MediaVaultView.class, args);
+        launch(args);
     }
 }
