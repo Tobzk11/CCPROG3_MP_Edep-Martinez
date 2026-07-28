ed063ffdc1d4e5441b11984ccf96b11910474e53
simonjaxith <simonjaxithmartinez77@gmail.com>
2026-07-28 17:10:37 +0800

    MediaVaultApp using pure Java

--- src/MediaVaultApp.java
+++ src/MediaVaultApp.java
@@ -1,23 +1,22 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 /**
  * Entry point of the MediaVault program. Its only job is to hand control to
  * the JavaFX runtime, which then constructs MediaVaultView and calls its
  * start method. The view is responsible for creating the Library, the
  * FileManager, and the MediaVaultController that ties them together.
  */
-
 public class MediaVaultApp extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainview.fxml"));
+    public void start(Stage primaryStage) {
+        MediaVaultView view = new MediaVaultView();
+        new MediaVaultController(view);
 
-        stage.setScene(new Scene(loader.load()));
-        stage.setTitle("MediaVault");
-        stage.show();
+        Scene scene = new Scene(view);
+        primaryStage.setTitle("MediaVault");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
